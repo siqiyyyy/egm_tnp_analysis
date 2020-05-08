5a7fa6167181a4c11fa0e9d2c569677b96aa08a7
syuan <syuan@cern.ch>
2020-05-08 12:40:53 -0400

    update the input defs in the tree

--- etc/inputs/tnpSampleDef.py
+++ etc/inputs/tnpSampleDef.py
@@ -1,28 +1,26 @@
 from libPython.tnpClassUtils import tnpSample
 
-### qll stat
-#eosDir1 = 'eos/cms/store/group/phys_egamma/tnp/80X/PhoEleIDs/v1/'
-#eosDir2 = 'eos/cms/store/group/phys_egamma/tnp/80X/PhoEleIDs/v2/'
-#eosDirREC = 'eos/cms/store/group/phys_egamma/tnp/80X/RecoSF/RECOSFs_2016/'
-#eosWinter17 = 'eos/cms/store/group/phys_egamma/tnp/80X/PhoEleIDs/Moriond17_v1/'
-eosMoriond18 = '/eos/cms/store/group/phys_egamma/soffi/TnP/ntuples_01292018/Moriond18_V1/'
-
-Moriond18_94X = {
+eosDir = 'root://cmseos.fnal.gov//store/user/syuan/TnP/2020-03-23/'
+Data2017 = {
     ### MiniAOD TnP for IDs scale factors
     'DY_madgraph'          : tnpSample('DY_madgraph',
-                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_all.root',
+                                       eosDir + 'merged_2017MC.root',
                                        isMC = True, nEvts =  -1 ),
-    'DY_madgraph_Moriond18' : tnpSample('DY_madgraph_Moriond18', 
-                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_all.root',
+    'DY_madgraph_oldMC'          : tnpSample('DY_madgraph',
+                                       'root://cmseos.fnal.gov//store/user/syuan/TnP/2020-04-23_oldMC/2017/merged_oldMC_filtered.root',
                                        isMC = True, nEvts =  -1 ),
-    'DY_amcatnlo_Moriond18' : tnpSample('DY_amcatnlo_Moriond18', 
-                                       eosMoriond18 + 'mc/TnPTree_DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_all.root',
-                                       isMC = True, nEvts =  -1 ),
+    'data' : tnpSample('data_Run2017' , eosDir + 'merged_2017data.root' , lumi = 41.5 ),
 
-    'data_Run2017B' : tnpSample('data_Run2017B' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunB.root' , lumi = 4.891 ),
-    'data_Run2017C' : tnpSample('data_Run2017C' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunC.root' , lumi = 9.9 ),
-    'data_Run2017D' : tnpSample('data_Run2017D' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunD.root' , lumi = 4.36 ),
-    'data_Run2017E' : tnpSample('data_Run2017E' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunE.root' , lumi = 9.53 ),
-    'data_Run2017F' : tnpSample('data_Run2017F' , eosMoriond18 + 'data/TnPTree_17Nov2017_RunF.root' , lumi = 13.96 ),
+    }
+
+Data2018 = {
+    ### MiniAOD TnP for IDs scale factors
+    'DY_madgraph'          : tnpSample('DY_madgraph',
+                                       eosDir + 'merged_2018MC.root',
+                                       isMC = True, nEvts =  -1 ),
+    'DY_madgraph_oldMC'          : tnpSample('DY_madgraph',
+                                       'root://cmseos.fnal.gov//store/user/syuan/TnP/2020-04-23_oldMC/2018/merged_oldMC.root',
+                                       isMC = True, nEvts =  -1 ),
+    'data' : tnpSample('data_Run2018' , eosDir + 'merged_2018data.root' , lumi = 59.7 ),
 
     }
